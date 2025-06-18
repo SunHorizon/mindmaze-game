b817fd7b57291099b969f4a088eddfaca787ce2b
Sarfaraz Syed <38136379+SunHorizon@users.noreply.github.com>
2025-06-18 10:46:51 -0400

    initial change

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>MindMaze!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +13,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    paddingTop: 50,
     alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#fff'
   },
+  title: {
+    fontSize: 32,
+    fontWeight: "bold",
+    color: "#333",
+  }
 });
